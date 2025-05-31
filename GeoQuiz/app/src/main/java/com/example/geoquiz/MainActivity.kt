cd4bb812cfd8887bf42ff271c424eb540dacb9e9
Daniel <lucky19manrus@gmail.com>
2025-05-31 11:29:21 +0700

    добавил ссылки на кнопки с вьюхи

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
