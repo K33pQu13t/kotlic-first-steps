e1bda77b674792e7805763128af33bba2a59e841
Daniel <lucky19manrus@gmail.com>
2025-06-13 11:00:20 +0700

    добавил конфигурирование RadioGroup в Activity

--- CashDeposit/app/src/main/java/com/example/cashdeposit/MainActivity.kt
+++ CashDeposit/app/src/main/java/com/example/cashdeposit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cashdeposit
 
 import android.os.Bundle
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        configureRadioGroups()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun configureRadioGroups() {
+        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            onRadioGroupCheckedChange(checkedId)
+        }
+    }
+
+    private fun onRadioGroupCheckedChange(checkedId: Int) {
+        when (checkedId) {
+            R.id.radio_deposit_3_months -> {
+                // Выбрано 3 месяца
+            }
+            R.id.radio_deposit_6_months -> {
+                // Выбрано 6 месяцев
+            }
+            R.id.radio_deposit_12_months -> {
+                // Выбран год
+            }
         }
     }
 }
