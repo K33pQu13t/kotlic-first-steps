32937c6e338b5df34d4e54e0ae843965fd012c36
Daniel <lucky19manrus@gmail.com>
2025-06-17 20:44:42 +0700

    добавил отображение меню с кнопкой добавления нового преступления. Пока без реализации добавления

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -32,6 +34,11 @@ class CrimeListFragment : Fragment() {
         callbacks = context as Callbacks
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -54,6 +61,12 @@ class CrimeListFragment : Fragment() {
                 updateUI(crimes)
             }
         }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
     }
 
     override fun onDetach() {
