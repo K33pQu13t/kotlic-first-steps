8eff368ed1dd5b1c92c2a4a43d987846b4ad0686
Daniel <lucky19manrus@gmail.com>
2025-06-17 16:06:32 +0700

    исправил анимацию чекбокса при повороте экрана

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -72,7 +72,10 @@ class CrimeFragment : Fragment() {
     private fun updateUI() {
         titleField.setText(crime.title)
         dateButton.text = crime.date.toString()
-        solvedCheckBox.isChecked = crime.isSolved
+        solvedCheckBox.apply {
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
+        }
     }
 
     companion object {
